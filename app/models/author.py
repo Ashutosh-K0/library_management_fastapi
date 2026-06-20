5cfce13f7d596367d6c426f1f3d7c8c1f8cf2853
Ashutosh-K0 <ashutosh.kumar2022b@vitstudent.ac.in>
2026-06-20 12:06:29 +0530

    Author Model Created

---
+++ app/models/author.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Integer, String, Column
+from app.database import Base
+
+class Author(Base):
+    __tablename__ = 'authors'
+    id = Column(Integer, primary_key= True)
+    name = Column(String(100), nullable= False)
+    email = Column(String(150), nullable=False, unique=True)
