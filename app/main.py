ce3886f90e4611d1dbb53aaff5384867c5b48a62
Ashutosh-K0 <ashutosh.kumar2022b@vitstudent.ac.in>
2026-06-25 00:51:53 +0530

    Routers for Author Category Books added

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from app.database import Base
+from app.models import author, book, borrow_record, category
+from app.database import engine
+from app.routers.author import router as author_router
+from app.routers.category import router as category_router
+from app.routers.book import router as book_router
+
+app = FastAPI()
+
+@app.get("/")
+def welcome():
+    return {'message': 'Library Management System'}
+
+Base.metadata.create_all(engine)
+
+app.include_router(author_router)
+app.include_router(category_router)
+app.include_router(book_router)
