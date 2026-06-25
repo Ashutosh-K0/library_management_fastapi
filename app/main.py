6015173c0935951eebc1e54a77581b6037a2003b
Ashutosh-K0 <ashutosh.kumar2022b@vitstudent.ac.in>
2026-06-25 23:03:09 +0530

    Added Borrow Record Router to main.py

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.database import engine
 from app.routers.author import router as author_router
 from app.routers.category import router as category_router
 from app.routers.book import router as book_router
+from app.routers.borrow_record import router as borrow_record_router
 
 app = FastAPI()
 
@@ -17,3 +18,4 @@ Base.metadata.create_all(engine)
 app.include_router(author_router)
 app.include_router(category_router)
 app.include_router(book_router)
+app.include_router(borrow_record_router)
